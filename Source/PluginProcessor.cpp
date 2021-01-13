71743e67411b7cabd5a9ab23a3749afab7662436
maurizdeb <mauriz.debari@gmail.com>
2021-01-13 11:54:47 +0100

    Add futura font by setting a default LNF

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -214,6 +214,7 @@ AudioProcessorEditor* CutTheMoogAudioProcessor::createEditor()
     builder->registerFactory ("PowerButton", &PowerButtonItem::factory);
     builder->registerLookAndFeel("Skeuomorphic", std::make_unique<foleys::Skeuomorphic>());
     builder->registerLookAndFeel("MYLNF", std::make_unique<OtherLookAndFeel>());
+    LookAndFeel::setDefaultLookAndFeel(&myLnf);
     return new foleys::MagicPluginEditor(magicState, BinaryData::CutTheMoog1_xml, BinaryData::CutTheMoog1_xmlSize, std::move(builder));
 }
 
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -127,6 +127,8 @@ private:
     void initTrimParams();
     void updateTrimParams();
     
+    OtherLookAndFeel myLnf;
+    
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CutTheMoogAudioProcessor)
 };
